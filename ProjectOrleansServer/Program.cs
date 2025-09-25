101c1f723464a522fced4a624a3cefddbcd0339e
huozk <huozk0804@126.com>
2025-09-25 10:08:31 +0800

    init

---
+++ ProjectOrleansServer/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using Orleans.Configuration;
+
+namespace Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHost(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHost(string[] args)
+        {
+            var builder = Host.CreateDefaultBuilder(args);
+            builder.UseOrleans(silo =>
+            {
+                silo.UseLocalhostClustering();
+                silo.UseDashboard(option =>
+                {
+                    option.Port = 10010;
+                });
+                silo.Configure<ClusterOptions>(option =>
+                {
+                    option.ClusterId = "clusterId";
+                    option.ServiceId = "serviceId";
+                });
+            });
+            return builder;
+        }
+    }
+}
---
+++ Remade.md
@@ -0,0 +1,5 @@
+# ProjectOrleansServer
+
+* ProjectOrleansServer
+* ProjectHotel
+* DB
